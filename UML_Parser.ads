90af60515b1f1352221ae1c1bd6945760881a861
Dimitri Belli <dimitribelli@hotmail.it>
2021-02-01 15:55:00 +0100

    Update UML_Parser.ads

--- UML_Parser.ads
+++ UML_Parser.ads
@@ -1,10 +1,10 @@
--- Ada local libraries
+-- local libraries
 with Ada.Text_IO;
 with Ada.Exceptions;
 with Ada.Characters.Handling;
 
 -- separate (configurations.Kernel)
-with Flags; use Flags;
+with Flags;
 
 package UML_Parser is
    -- enable the use of the imported libraries
@@ -12,6 +12,8 @@ package UML_Parser is
    use Ada.Exceptions;
    use Ada.Characters.Handling;
 
+   use Flags;
+   
    -- parses the uml file indentified by the given name
    -- and creates the corresponding uml object in "This_UML_Object"
    procedure Parse (Uml_File_Name: String);
